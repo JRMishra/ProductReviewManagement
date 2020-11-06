a844e89cebc6fbd04ede48071f7d42fa96a3febd
JR Mishra <jrmnitdgp@gmail.com>
2020-11-06 18:57:16 +0530

    [JR Mishra]Add. Retrieve reviews with rating greater than specifie ratings and having ProductIds among given ids

--- ProductReviewManagement/ReviewAdapter.cs
+++ ProductReviewManagement/ReviewAdapter.cs
@@ -19,7 +19,20 @@ namespace ProductReviewManagement
 
         public static IList<ProductReviewModel> GetReviews_AboveSpecifiedRating_HavingGivenIds(int rating, params int[] ids)
         {
-            return new List<ProductReviewModel>();
+            ProductReviewData reviewData = new ProductReviewData();
+            var productReviews = (from review in reviewData.ProductReviewList
+                                 where review.Rating > 3
+                                 select review);
+
+            List<ProductReviewModel> finalProductReview = new List<ProductReviewModel>();
+            foreach(int id in ids)
+            {
+                var products = (from review in productReviews
+                                where review.ProductId == id
+                                select review);
+                finalProductReview.AddRange(products.ToList());
+            }
+            return finalProductReview;
         }
     }
 }
