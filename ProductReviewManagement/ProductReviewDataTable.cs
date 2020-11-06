96ba4a8c5b2405dd9e7ff6b84ac4157a70e44964
JR Mishra <jrmnitdgp@gmail.com>
2020-11-07 00:20:50 +0530

    [JR Mishra]Add. Testcases for eighth UC

---
+++ ProductReviewManagement/ProductReviewDataTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    public class ProductReviewDataTable
+    {
+        public DataTable dataTableStorage = new DataTable();
+    }
+}
---
+++ TestProductReviewManagement/UnitTest2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductReviewManagement;
+using System.Collections.Generic;
+
+
+namespace TestProductReviewManagement
+{
+    [TestClass]
+    class UnitTest2
+    {
+        [TestMethod]
+        public void ProductReviewDataTable_WhenCreated_ShouldAdd25Reviews()
+        {
+            //Arrange
+            ProductReviewDataTable productReviewDataTable = new ProductReviewDataTable();
+            //Act
+            int actualCount = productReviewDataTable.dataTableStorage.Rows.Count;
+            //Assert
+            Assert.AreEqual(25, actualCount);
+        }
+
+    }
+}
