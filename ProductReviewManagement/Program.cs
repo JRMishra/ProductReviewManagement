553ebb1bf62fae8f56991ffd2d3b5bbcab3f0e24
JR Mishra <jrmnitdgp@gmail.com>
2020-11-06 16:35:25 +0530

    [JR Mishra]Add. Project Initialized

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management System");
+            Console.WriteLine("===========================================");
+        }
+    }
+}
